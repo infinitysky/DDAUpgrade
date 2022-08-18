db34511f7c02fd2235b2007ac048e6f3c4df72f5
infinitysky <dreamli1121@gmail.com>
2022-08-18 10:19:43 +1000

    project init

---
+++ ZiiPOS Retail UpgradeTool/ZiiPOS Retail UpgradeTool/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace ZiiPOS_Retail_UpgradeTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.upgradeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // upgradeButton
+            // 
+            this.upgradeButton.Location = new System.Drawing.Point(84, 49);
+            this.upgradeButton.Name = "upgradeButton";
+            this.upgradeButton.Size = new System.Drawing.Size(142, 23);
+            this.upgradeButton.TabIndex = 0;
+            this.upgradeButton.Text = "Upgrade to Latest version";
+            this.upgradeButton.UseVisualStyleBackColor = true;
+            this.upgradeButton.Click += new System.EventHandler(this.upgradeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 130);
+            this.Controls.Add(this.upgradeButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button upgradeButton;
+    }
+}
+
---
+++ ZiiPOS Retail UpgradeTool/ZiiPOS Retail UpgradeTool/Form1.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZiiPOS_Retail_UpgradeTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class ZiiPOSRetailResponse
+        {
+            public DateTimeOffset Date { get; set; }
+            public int TemperatureCelsius { get; set; }
+
+        }
+       
+        private void upgradeButton_Click(object sender, EventArgs e)
+        {
+   
+            String url = "https://wombat-api.ziicloud.com/api/vs/client-version/version?clientName=Zii.Retail_Classic&version=1";
+            var jsoninfo ="";
+
+            try
+            {
+
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                HttpWebResponse response1 = (HttpWebResponse)request.GetResponse();
+                request.Timeout = 10;
+                WebHeaderCollection header = response1.Headers;
+
+                var encoding = ASCIIEncoding.UTF8;
+                //using (var reader = new System.IO.StreamReader(response.GetResponseStream(), encoding))
+                using (var reader = new System.IO.StreamReader(response1.GetResponseStream(), encoding))
+                {
+                    string responseText = reader.ReadToEnd();
+                    jsoninfo = responseText;
+                    dynamic dyn = JsonConvert.DeserializeObject(jsoninfo);
+
+
+
+                
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace.ToString());
+                //Console.WriteLine(ex.StackTrace.ToString());
+            }
+
+
+        }
+
+
+
+    }
+}
